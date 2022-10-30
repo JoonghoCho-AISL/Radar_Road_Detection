6da24788d5f2daf4bbc38e001d8ae00b0ef07428
JoonghoCho-AISL <joongho.sejong@gamil.com>
2022-10-30 12:24:26 +0000

    pca

--- base_model.py
+++ base_model.py
@@ -8,6 +8,10 @@ from acconeer.exptool import a121
 from sklearn.model_selection import train_test_split
 import argparse
 
+import pickle as pkl
+
+from sklearn.decomposition import PCA
+
 from models import basemodel
 
 
@@ -61,8 +65,11 @@ def concat(arr : list):
         temp = np.concatenate((temp, arr[i]), axis = 0)
     return temp
 
-def saveplot(history, plot_name):
-    plot_name = './basemodel/' + str(plot_name) + '.png'
+def saveplot(history, plot_name, pca):
+    if pca:
+        plot_name = './basemodel/' + str(plot_name) + 'pca' + '.png'
+    else:
+        plot_name = './basemodel/' + str(plot_name) + '.png'
     plt.subplot(4,1,1)
     plt.plot(history['val_accuracy'])
     plt.title('val_accuracy')
@@ -77,16 +84,21 @@ def saveplot(history, plot_name):
     plt.title('loss')
     plt.savefig(plot_name, dpi = 300)
 
+def var_argmax(arr):
+    return np.argmax(arr, axis = 0)
+
 def main():
     
     parser = argparse.ArgumentParser(description = 'selt data and gpu')
     parser.add_argument('-g', '--gpu', action = 'store', default = '3')
+    parser.add_argument('-p', '--pca', action = 'store_true')
     parser.add_argument('-d', '--data', action = 'store')
-    parser.add_argument('-m', '--model', action = 'store')
+    parser.add_argument('-m', '--model', action = 'store', default = 'base')
     args = parser.parse_args()
 
     gpu = int(args.gpu)
     data = args.data
+    Pca = args.pca
     sel_model = args.model
 
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -118,6 +130,8 @@ def main():
     MAX = np.max([np.max(asphalt_mean), np.max(sidewalk_mean), np.max(floor_mean), np.max(ground_mean)])
     MAX_origin = np.max([np.max(asphalt_abs), np.max(sidewalk_abs), np.max(floor_abs), np.max(ground_abs)])
 
+    random_seed = 33
+
     norm_asphalt_mean = asphalt_mean / MAX
     norm_sidewalk_mean = sidewalk_mean / MAX
     norm_floor_mean = floor_mean / MAX
@@ -128,52 +142,100 @@ def main():
     norm_floor_var = np.var(floor_abs/ MAX_origin, axis = 1)
     norm_ground_var = np.var(ground_abs / MAX_origin, axis = 1)
 
-    asphalt_mean_X = add_label(norm_asphalt_mean[:,:3], label2idx_Dict['asphalt'])
-    floor_mean_X = add_label(norm_floor_mean[:,:3], label2idx_Dict['floor'])
-    sidewalk_mean_X = add_label(norm_sidewalk_mean[:,:3], label2idx_Dict['sidewalk'])
-    ground_mean_X = add_label(norm_ground_mean[:,:3], label2idx_Dict['ground'])
-
-    asphalt_var_X = add_label(norm_asphalt_var[:,:4], label2idx_Dict['asphalt'])
-    floor_var_X = add_label(norm_floor_var[:,:4], label2idx_Dict['floor'])
-    sidewalk_var_X = add_label(norm_sidewalk_var[:,:4], label2idx_Dict['sidewalk'])
-    ground_var_X = add_label(norm_ground_var[:,:4], label2idx_Dict['ground'])
-
-    asphalt_X = add_label(np.concatenate((norm_asphalt_mean[:,:3], norm_asphalt_var[:,:4]), axis = 1), label2idx_Dict['asphalt'])
-    floor_X = add_label(np.concatenate((norm_floor_mean[:,:3], norm_floor_var[:,:4]), axis = 1), label2idx_Dict['floor'])
-    sidewalk_X = add_label(np.concatenate((norm_sidewalk_mean[:,:3], norm_sidewalk_var[:, :4]), axis = 1), label2idx_Dict['sidewalk'])
-    ground_X = add_label(np.concatenate((norm_ground_mean[:,:3], norm_ground_var[:,:4]), axis = 1), label2idx_Dict['ground'])
-
-    meanData = concat([asphalt_mean_X, floor_mean_X, sidewalk_mean_X, ground_mean_X])
-    varData = concat([asphalt_var_X, floor_var_X, sidewalk_var_X, ground_var_X])
-    Data = concat([asphalt_X, floor_X, sidewalk_X, ground_X])
-
-    mean_Y = tf.one_hot(meanData[:,-1], len(label2idx_Dict)).numpy()
-    var_Y = tf.one_hot(varData[:,-1], len(label2idx_Dict)).numpy()
-    Y = tf.one_hot(Data[:,-1], len(label2idx_Dict)).numpy()
-
-    random_seed = 33
-
     if data == 'mean':
-        X_train, X_test, Y_train, Y_test = train_test_split(meanData[: , :-1], mean_Y,  random_state = random_seed)
+    
+        if not Pca:    
+            asphalt_mean_X = add_label(norm_asphalt_mean[:,:3], label2idx_Dict['asphalt'])
+            floor_mean_X = add_label(norm_floor_mean[:,:3], label2idx_Dict['floor'])
+            sidewalk_mean_X = add_label(norm_sidewalk_mean[:,:3], label2idx_Dict['sidewalk'])
+            ground_mean_X = add_label(norm_ground_mean[:,:3], label2idx_Dict['ground'])
+        
+        else:
+            pre_Data =concat([norm_asphalt_mean, norm_sidewalk_mean, norm_floor_mean, norm_ground_mean])
+            pca = PCA(n_components = 'mle')
+            pca.fit(pre_Data)
+
+            asphalt_mean_X = add_label(pca.transform(norm_asphalt_mean), label2idx_Dict['asphalt'])
+            floor_mean_X = add_label(pca.transform(norm_floor_mean), label2idx_Dict['floor'])
+            sidewalk_mean_X = add_label(pca.transform(norm_sidewalk_mean), label2idx_Dict['sidewalk'])
+            ground_mean_X = add_label(pca.transform(norm_ground_mean), label2idx_Dict['ground'])
+
+            pca_path = './pca/mean_pca.pkl'
+
+        Data = concat([asphalt_mean_X, floor_mean_X, sidewalk_mean_X, ground_mean_X])
+
+        Y = tf.one_hot(Data[:,-1], len(label2idx_Dict)).numpy()
+
     elif data == 'var':
-        X_train, X_test, Y_train, Y_test = train_test_split(varData[: , :-1], var_Y, random_state = random_seed)
+
+        if not Pca:
+            asphalt_var_X = add_label(norm_asphalt_var[:,:4], label2idx_Dict['asphalt'])
+            floor_var_X = add_label(norm_floor_var[:,:4], label2idx_Dict['floor'])
+            sidewalk_var_X = add_label(norm_sidewalk_var[:,:4], label2idx_Dict['sidewalk'])
+            ground_var_X = add_label(norm_ground_var[:,:4], label2idx_Dict['ground'])
+        
+        else:
+            pre_Data = concat([norm_asphalt_var, norm_sidewalk_var, norm_floor_var, norm_ground_var])
+            pca = PCA(n_components = 'mle')
+            pca.fit(pre_Data)
+
+            asphalt_var_X = add_label(pca.transform(norm_asphalt_var), label2idx_Dict['asphalt'])
+            floor_var_X = add_label(pca.transform(norm_floor_var), label2idx_Dict['floor'])
+            sidewalk_var_X = add_label(pca.transform(norm_sidewalk_var), label2idx_Dict['sidewalk'])
+            ground_var_X = add_label(pca.transform(norm_ground_var), label2idx_Dict['ground'])
+
+            pca_path = './pca/var_pca.pkl'
+        Data = concat([asphalt_var_X, floor_var_X, sidewalk_var_X, ground_var_X])
+
+        Y = tf.one_hot(Data[:,-1], len(label2idx_Dict)).numpy()
+
     elif data == 'all':
-        X_train, X_test, Y_train, Y_test = train_test_split(Data[:,:-1], Y, random_state = random_seed)
-    # mean_X_train, mean_X_test, mean_Y_train, mean_Y_test = train_test_split(meanData[: , :-1], mean_Y,  random_state = random_seed)
-    # var_X_train, var_X_test, var_Y_train, var_Y_test = train_test_split(varData[: , :-1], var_Y, random_state = random_seed)
-    # X_train, X_test, Y_train, Y_test = train_test_split(Data[:,:-1], Y, random_state = random_seed)
-    
-    
+        
+        if not Pca:
+            asphalt_X = add_label(np.concatenate((norm_asphalt_mean[:,:3], norm_asphalt_var[:,:4]), axis = 1), label2idx_Dict['asphalt'])
+            floor_X = add_label(np.concatenate((norm_floor_mean[:,:3], norm_floor_var[:,:4]), axis = 1), label2idx_Dict['floor'])
+            sidewalk_X = add_label(np.concatenate((norm_sidewalk_mean[:,:3], norm_sidewalk_var[:, :4]), axis = 1), label2idx_Dict['sidewalk'])
+            ground_X = add_label(np.concatenate((norm_ground_mean[:,:3], norm_ground_var[:,:4]), axis = 1), label2idx_Dict['ground'])
+        
+        else:
+            asphalt = np.concatenate([norm_asphalt_mean, norm_asphalt_var], axis = 1)
+            floor = np.concatenate([norm_floor_mean, norm_floor_var], axis = 1)
+            sidewalk = np.concatenate([norm_sidewalk_mean, norm_sidewalk_var], axis = 1)
+            ground = np.concatenate([norm_ground_mean, norm_ground_var], axis = 1)
+            pre_Data = concat([asphalt, floor, sidewalk, ground])
+
+            pca = PCA(n_components='mle')
+            pca.fit(pre_Data)
+
+            asphalt_X = add_label(pca.transform(asphalt), label2idx_Dict['asphalt'])
+            floor_X = add_label(pca.transform(floor), label2idx_Dict['floor'])
+            sidewalk_X = add_label(pca.transform(sidewalk), label2idx_Dict['sidewalk'])
+            ground_X = add_label(pca.transform(ground), label2idx_Dict['ground'])
+            
+            pca_path = './pca/all_pca.pkl'
+
+        Data = concat([asphalt_X, floor_X, sidewalk_X, ground_X])
+  
+        Y = tf.one_hot(Data[:,-1], len(label2idx_Dict)).numpy()
+
+    X_train, X_test, Y_train, Y_test = train_test_split(Data[:,:-1], Y, random_state = random_seed)
+
+    if pca:
+        with open(pca_path, 'wb') as pickle_file:
+            pkl.dump(pca, pickle_file)
+
     if sel_model == 'base':
         model = basemodel()
         history = train(model, X_train, Y_train, X_test, Y_test)
-        saveplot(history.history, data)
+        saveplot(history.history, data, Pca)
+
     elif sel_model == 'rf':
         import tensorflow_decision_forests as tfdf
         model = tfdf.keras.RandomForestModel()
         # history = train(model, X_train, np.argmax(Y_train, axis = 1), X_test, np.argmax(Y_test, axis = 1))
         history = model.fit(X_train, np.argmax(Y_train, axis = 1), validation_data = (X_test, np.argmax(Y_test, axis = 1)))
         model.evaluate(X_test, np.argmax(Y_test, axis = 1))
+
     else: 
         pass
     # history = train(model, mean_X_train, mean_Y_train, mean_X_test, mean_Y_test)
