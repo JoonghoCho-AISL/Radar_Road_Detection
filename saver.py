d3e191c63012f165945abf75b9b9c7a8b1a367d0
JoonghoCho-AISL <joongho.sejong@gmail.com>
2023-09-27 00:05:36 +0000

    27 Sep

--- saver.py
+++ saver.py
@@ -7,16 +7,19 @@ from tqdm import tqdm
 import json
 import os
 import argparse
+from MobiusAPI import http_post_get
 # import tensorflow as tf
 # from models import basemodel_rasp
 # from pub_sub import publishing
 # from sklearn.decomposition import PCA
 
+MAX = 20655.93
+
 class radar_raspi():
     def __init__(self,
                     ip_address='127.0.0.1',
-                    start_point = 160,
-                    num_points = 301,
+                    start_point = 16,
+                    num_points = 32,
                     hwaas = 8,
                     sweep_per_frame = 10,
                     frame_rate = 10
@@ -25,10 +28,10 @@ class radar_raspi():
 
         self.client.connect()
         #start_distance = start_point * 2.5mm
-        start_point = 160 # 400mm
+        # start_point = 160 # 400mm
         #end_distance = start_point * 2.5mm + num_points * 2.5mm 
         # num_points = (end_dis - start_point * 2.5) / 2.5 + 1
-        num_points = 301 # 1,150mm
+        # num_points = 301 # 1,150mm
 
         sensor_config = a121.SensorConfig(
             subsweeps=[
@@ -64,21 +67,18 @@ class radar_raspi():
     def disconnect(self):
         self.client.disconnect()
 
-def send_mobius(data, ip):
-    pass
-
 def main():
     parser = argparse.ArgumentParser(description = 'select ip, road name, save or post')
     parser.add_argument('-i', '--ip', action = 'store', default = '192.168.222.144')
-    parser.add_argument('-c', '--road_class', action = 'store')
+    parser.add_argument('-c', '--road_class', action = 'store', default = None)
     parser.add_argument('-s', '--save', action = 'store_true')
-    parser.add_argument('-n', '--number', action = 'store')
+    parser.add_argument('-n', '--number', action = 'store', default = None)
     args = parser.parse_args()
 
     ip = args.ip
     road = args.road_class
     save = args.save
-    number = int(args.number)
+    number = args.number
 
     client = radar_raspi(ip_address=ip)
     print('clinet_start')
@@ -86,12 +86,19 @@ def main():
         folder_path = 'e-scooter/'
         file_path = folder_path + road
         temp = list()
-        for i in tqdm(range(number)):
+        for i in tqdm(range(int(number))):
             data = client.read_mean_var()
             temp.append(data)
         save_data = np.array(temp)
         np.save(file_path, save_data)
-
+    else:
+        while True:
+            data = client.read_mean_var()
+            send_data = json.dumps(data.tolist())
+            URI = '/Mobius/PM_MFBE29/radarSensor/rawData'
+            AE_ID = 'PM_MFBE29'
+            http_post_get.mobius_post(URI, AE_ID, AE_ID, send_data)
+            print('pub')
     client.disconnect()
 
 if __name__ == '__main__':
